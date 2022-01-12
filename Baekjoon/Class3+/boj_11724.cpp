28b70beccc28fbd88081562e6b594d035f46924d
skennetho <thrkd613@naver.com>
2022-01-12 22:17:19 +0900

    solve baekjoon Class3+ and StepByStep

---
+++ Baekjoon/Class3+/boj_11724.cpp
@@ -0,0 +1,92 @@
+// //https://www.acmicpc.net/problem/11724
+// //연결 요소의 개수
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// void dfsMakeConnectedNodeIntoFalse(vector<vector<bool>>& arr, int x) {
+//     for (int i = 0; i < arr[x].size(); i++) {
+//         if (arr[x][i] == true) {
+//             arr[x][i] = false;
+//             dfsMakeConnectedNodeIntoFalse(arr, i);
+//         }
+//     }
+// }
+
+// int main() {
+//     int n; //정점의 개수
+//     int m; //간선의 개수
+//     scanf("%d %d", &n, &m);
+
+//     vector<vector<bool>> arr(n,vector<bool>(n, false));
+//     for (int i = 0; i < n; i++) {
+//         arr[i][i] = true;
+//     }
+
+//     int u, v;
+//     for (int i = 0; i < m; i++) {
+//         scanf("%d %d", &u, &v);
+//         arr[v - 1][u - 1] = true;
+//         arr[u - 1][v - 1] = true;
+//     }
+
+//     int count = 0;
+//     for (int i = 0; i < n; i++) {
+//         if (arr[i][i]) {
+//             dfsMakeConnectedNodeIntoFalse(arr, i);
+//             count++;
+//         } 
+//     }
+//     printf("%d\n", count);
+// }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int arr[1010];
+int ranker[1010];
+
+int find(int n){
+    if(arr[n]!=n) {
+        return arr[n] = find(arr[n]);
+    }
+    else {
+        return n;
+    }
+}
+void uni(int a,int b){
+    a = find(a);
+    b = find(b);
+
+    if(a==b) return;
+
+    if(ranker[a]<ranker[b]){
+        arr[b]=a;
+    }
+    else {
+        arr[a]=b;
+        if(ranker[a]==ranker[b]) ranker[a]++;
+    }
+}
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);    
+
+    int n,m;
+    cin >> n >> m;
+
+    for(int i=1;i<=n;i++) {
+        arr[i]=i;
+        ranker[i]=0;
+    }
+    for(int i=0;i<m;i++) {
+        int u,v;
+        cin >> u >> v;
+        uni(u,v);
+    }
+    int count = 0;
+    for(int i=1;i<=n;i++) if(arr[i]==i) count++;
+    cout << count;
+    return 0;
+}
---
+++ Baekjoon/StepByStep/3_for/boj_15552.cpp
@@ -0,0 +1,16 @@
+//https://www.acmicpc.net/problem/15552
+// ºü¸¥ A+B
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a+b);
+    }
+}
